4ea2d415d17a4c62a143dc8ff5f476ec7ff6838c
Kousick S <skousick57@gmail.com>
2025-11-23 17:26:09 +0530

    v0.4

--- app.py
+++ app.py
@@ -1,63 +1,16 @@
 import streamlit as st
 
+st.set_page_config(page_title="Watsonx Orchestrate Chat", layout="wide")
 
+st.title("Watsonx Orchestrate Chat (Iframe Mode)")
 
-st.title("Watsonx Orchestrate Embedded Chat")
-
-
-
-embed_code = """
-
-<div id="WACCRoot" style="width:100%; height:700px;"></div>
-
-
-
-<script>
-
-  window.wxOConfiguration = {
-
-    orchestrationID: "89fd1cc02bd2458cad21c01f2fbfd855_2b02d943-eeab-4874-aeb2-af5c046e113a",
-
-    hostURL: "https://au-syd.watson-orchestrate.cloud.ibm.com",
-
-    rootElementID: "WACCRoot",
-
-    deploymentPlatform: "ibmcloud",
-
-    crn: "crn:v1:bluemix:public:watsonx-orchestrate:au-syd:a/89fd1cc02bd2458cad21c01f2fbfd855:2b02d943-eeab-4874-aeb2-af5c046e113a::",
-
-    chatOptions: {
-
-        agentId: "d8730186-1a77-4834-8e84-524d160c2ad2",
-
-        agentEnvironmentId: "db284261-1a23-463d-9c41-79e17c837276"
-
-    }
-
-  };
-
-
-
-  const script = document.createElement('script');
-
-  script.src = window.wxOConfiguration.hostURL + "/wxochat/wxoLoader.js?embed=true";
-
-  script.onload = () => {
-
-      setTimeout(() => {
-
-          wxoLoader.init();
-
-      }, 100);
-
-  };
-
-  document.head.appendChild(script);
-
-</script>
-
+iframe_code = """
+<iframe
+    src="https://au-syd.watson-orchestrate.cloud.ibm.com/wxochat/?agentId=d8730186-1a77-4834-8e84-524d160c2ad2&agentEnvironmentId=db284261-1a23-463d-9c41-79e17c837276&embed=true"
+    width="100%"
+    height="800"
+    style="border: none;"
+></iframe>
 """
 
-
-
-st.components.v1.html(embed_code, height=800, scrolling=True)
+st.components.v1.html(iframe_code, height=820)
