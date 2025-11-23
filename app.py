181c6ed734ee1baed6a5e1651bd0fa74849b32d3
Kousick S <skousick57@gmail.com>
2025-11-23 17:07:15 +0530

    v0.2

--- app.py
+++ app.py
@@ -13,19 +13,18 @@ embed_code = """
     deploymentPlatform: "ibmcloud",
     crn: "crn:v1:bluemix:public:watsonx-orchestrate:au-syd:a/89fd1cc02bd2458cad21c01f2fbfd855:2b02d943-eeab-4874-aeb2-af5c046e113a::",
     chatOptions: {
-        agentId: "d8730186-1a77-4834-8e84-524d160c2ad2",
-        agentEnvironmentId: "db284261-1a23-463d-9c41-79e17c837276"
+        agentId: "d8730186-1a77-4834-8e84-524d160c2ad2", 
+        agentEnvironmentId: "db284261-1a23-463d-9c41-79e17c837276",
     }
   };
-
   setTimeout(function () {
     const script = document.createElement('script');
-    script.src = window.wxOConfiguration.hostURL + "/wxochat/wxoLoader.js?embed=true";
+    script.src = `${window.wxOConfiguration.hostURL}/wxochat/wxoLoader.js?embed=true`;
     script.addEventListener('load', function () {
         wxoLoader.init();
     });
     document.head.appendChild(script);
-  }, 0);
+  }, 0);                     
 </script>
 """
 
